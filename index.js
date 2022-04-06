ee91f6cf8b4902a6e52cc6308a5538b624f7953c
Alejandro <alexsagarnaga@gmail.com>
2022-04-06 09:53:09 -0500

    tested serverer to be up with /health route, server is up returing all is well

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // create the express server here
 const express = require('express');
 const server = express();
-const { client } = require("./db");
 const apiRouter = require("./api");
 const morgan = require('morgan');
 require('dotenv').config()
@@ -13,6 +12,24 @@ server.use(express.json());
 
 
 server.use("/api", apiRouter);
+
+server.use((req, res, next) => {
+  console.log("Start up");
+  console.log(req.body);
+  console.log("End");
+
+  next();
+});
+
+server.use("/api", apiRouter);
+
+server.get('/health', (req, res, next) => {
+  res.send(`
+    <body style="background: ${ req.params.color };">
+      <h1> All is Well </h1>
+    </body>
+  `);
+});
 
 
 
