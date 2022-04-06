1796e58aa6efbf05902f4e39ff32837529dcbd04
Alejandro <alexsagarnaga@gmail.com>
2022-04-05 21:55:17 -0500

    created express server and created api files needed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // create the express server here
+const express = require('express');
+const server = express();
+const { client } = require("./db");
+const apiRouter = require("./api");
+const morgan = require('morgan');
+require('dotenv').config()
+const {PORT = 3000}=process.env;
+
+server.use(morgan("dev"));
+
+server.use(express.json());
+
+
+server.use("/api", apiRouter);
+
+
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
