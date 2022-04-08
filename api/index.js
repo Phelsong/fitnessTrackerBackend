28b27d3639c3ebf8586d644479d4df1a07a727e2
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-08 10:47:27 -0500

    setting up routers

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
-// create an api router
-// attach other routers from files in this api directory (users, activities...)
-// export the api router
+// -------------- READ ME ----------------------------------------
+/*
+
+
+*/
+//----------------------------------------------------------------
 const express = require("express");
 const apiRouter = express.Router();
 require('dotenv').config()
@@ -8,6 +11,63 @@ const usersRouter = require("./users");
 const activitiesRouter = require("./activities");
 const routinesRouter = require("./routines");
 const routine_activitiesRouter = require("./routine_activities");
+const {
+    createUser, createActivity, createRoutine, getAllActivities, getAllRoutines, addActivityToRoutine, getRoutinesWithoutActivities, getActivitiesById, getActivitiesbyRoutine
+  } = require('./client');
+
+//----------------------------------------------------------------
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET } = process.env
+//----------------------------------------------------------------
+
+
+//----------------------------------------------------------------
+apiRouter.use(async (req, res, next) => {
+    const prefix = 'Bearer ';
+    const auth = req.header('Authorization');
+  
+    if (!auth) { // nothing to see here
+      next();
+    } else if (auth.startsWith(prefix)) {
+      const token = auth.slice(prefix.length);
+  
+      try {
+        const { id } = jwt.verify(token, JWT_SECRET);
+  
+        if (id) {
+          req.user = await getUserById(id);
+          next();
+        }
+      } catch ({ name, message }) {
+        next({ name, message });
+      }
+    } else {
+      next({
+        name: 'AuthorizationHeaderError',
+        message: `Authorization token must start with ${ prefix }`
+      });
+    }
+  });
+//----------------------------------------------------------------
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+
+  next();
+});
+//-------------------------------------------
+apiRouter.use('/users', usersRouter);
+apiRouter.use('/activities', activitiesRouter); 
+apiRouter.use('/routines', routinesRouter);
+//----------------------------------------------------------------
+//.. Error Handler
+apiRouter.use((error, req, res, next) => {
+    res.send({
+      name: error.name,
+      message: error.message
+    });
+  });
 
 
 
@@ -20,7 +80,5 @@ const routine_activitiesRouter = require("./routine_activities");
 
 
 
-
-
-
-module.exports = apiRouter
+//-------------------------------------------------------------------
+module.exports = apiRouter;
--- db/index.js
+++ db/index.js
@@ -1,1 +1,8 @@
 // require and re-export all files in this db directory (users, activities...)
+const {
+    client,  createUser, createActivity, createRoutine, getAllActivities, getAllRoutines, addActivityToRoutine, getRoutinesWithoutActivities, getActivitiesById, getActivitiesbyRoutine
+  } = require('./client');
+
+module.exports = {
+    createUser, createActivity, createRoutine, getAllActivities, getAllRoutines, addActivityToRoutine, getRoutinesWithoutActivities, getActivitiesById, getActivitiesbyRoutine
+  };
